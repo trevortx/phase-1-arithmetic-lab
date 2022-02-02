54b01c9dd295b3c23f07f9aa2077e91b2c2ae2e8
Trevor Vardeman <trevortx@gmail.com>
2022-02-01 22:07:41 -0800

    commit tree 624068d38fe832146d039e662d2d1fd8b2314bbf

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31, num2 = 2, num3 = 22, num4 = 6;
 
-// Write your code here
+let multiply = (num1 * num2);
+
+let random = Math.ceil(Math.random() * 10);
+
+let mod = (num3 % num4);
+
+let max = Math.max(1, 2, 5, 16, 20);
